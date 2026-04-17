7d9b3a9c7f0a39c46b111d901e7a30ae98f40be7
Seamus Sloan <seamus.sloan@gmail.com>
2026-04-17 18:27:49 -0400

    feat: get mobile (android) working

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,11 +11,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .and_then(|value| value.parse::<u16>().ok())
         .unwrap_or(3000);
 
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive("omnibus=debug".parse()?)
+                .add_directive("tower_http=debug".parse()?),
+        )
+        .init();
+
     let pool = db::init_db(&database_url).await?;
     let state = backend::AppState::new(pool);
     let app = backend::router(state);
 
-    let address = SocketAddr::from(([127, 0, 0, 1], port));
+    let address = SocketAddr::from(([0, 0, 0, 0], port));
     let listener = tokio::net::TcpListener::bind(address).await?;
     println!("Server running at http://{}", listener.local_addr()?);
 
